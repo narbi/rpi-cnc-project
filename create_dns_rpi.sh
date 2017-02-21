36999f7acade62e82e9c5f7a2a7c816c7e5bfb7d
Christina <skouloudi@gmail.com>
2017-02-21 12:05:18 +0200

    also command for access point

--- create_dns_rpi.sh
+++ create_dns_rpi.sh
@@ -7,14 +7,17 @@
 if [[ "$(ping -c 1 8.8.8.8 | grep '100% packet loss' )" != "" ]]; then
     echo "Internet isn't present, please connect and try again"
     exit 1
-sudo apt-get update
+sudo apt-get update -y
 
 #static IP addr to work as DHCP server
 echo -e "What is the static IP you would you like to give to this device?" 
 read staticip
 
 # Install bind9 for DNS server
-sudo apt-get install bind9 bind9-docs dnsutils
+sudo apt-get install bind9 bind9-docs dnsutils -y
+
+# Install dnsmasq for DNS server and Access Point 
+sudo apt-get install dnsmasq hostapd -y
 
 # Checking the Status of the Server
 if [[ "$(service bind9 status| grep 'ok')" != "" ] then
